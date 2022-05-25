3f0ea87aac76cfe261afc375c577fcc14a3ff80a
“tlvu93” <101603072+tlvu93@users.noreply.github.com>
2022-05-25 21:00:14 +0200

    WIP

--- frontend/src/Router.js
+++ frontend/src/Router.js
@@ -9,14 +9,24 @@ const Router = () => (
   <BrowserRouter>
     <Routes>
       <Route exact path='/' element={<HomePage />} />
+      <Route path='pokemon' element={<PokemonListPage />} />
       <Route path='pokemon' element={<PokemonListPage />}>
         <Route path=':id' element={<DetailedPage />}>
           <Route path=':info' element={<DetailedInfoPage />} />
         </Route>
       </Route>
+
       <Route element={<NotFoundPage />} />
     </Routes>
   </BrowserRouter>
 );
 
 export default Router;
+
+// TODO: Decide to use Outlet, if not use direct paths and delete this comment block
+// eslint-disable-next-line no-lone-blocks
+{
+  /* TODO: Decide to use Outlet, if not use direct paths
+  <Route path='pokemon/:id' element={<DetailedPage />} />
+  <Route path='pokemon/:id/:info' element={<DetailedInfoPage />} /> */
+}
